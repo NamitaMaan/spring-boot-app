0feb7dea6a58e0b7f546e1b6119bdc948507e893
Namita Maan <33954034+NamitaMaan@users.noreply.github.com>
2021-12-14 14:20:34 +0530

    Delete product directory

--- product/src/main/java/com/ibm/exception/IdnotMatchException.java
+++
@@ -1,6 +0,0 @@
-package com.ibm.exception;
-
-public class IdnotMatchException extends RuntimeException{
-
-}
-
--- product/src/main/java/com/ibm/exception/ProductsNotFoundException.java
+++
@@ -1,6 +0,0 @@
-package com.ibm.exception;
-
-public class ProductsNotFoundException extends RuntimeException{
-
-}
-
